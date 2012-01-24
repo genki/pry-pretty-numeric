e49ad0b7cc3eeec66749682e765365f19063402d
瀧内 元気 <genki@s21g.com>
2012-01-25 02:09:02 +0900

    Fixed a bug

--- lib/pry-pretty-numeric.rb
+++ lib/pry-pretty-numeric.rb
@@ -4,8 +4,8 @@ require "pp"
 class Numeric
   def pretty_inspect
     integral, fractional = super.split('.', 2)
-    integral = integral.reverse.gsub(/.{3}(?!$)/, '\&_').reverse
-    fractional = fractional.gsub(/.{3}/, '\&_') if fractional
+    integral = integral.reverse.gsub(/\d{3}(?!$)/, '\&_').reverse
+    fractional = fractional.gsub(/\d{3}(?!$)/, '\&_') if fractional
     [integral, fractional].compact.join('.')
   end
 end
--- lib/pry-pretty-numeric/version.rb
+++ lib/pry-pretty-numeric/version.rb
@@ -1,5 +1,5 @@
 class Pry
   module PrettyNumeric
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
   end
 end
